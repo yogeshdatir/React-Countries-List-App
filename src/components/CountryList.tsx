f9af2ccefcd2e0f9cfb9de332f29694579dbc56e
yogeshdatir <yogdat@gmail.com>
2021-04-02 22:58:14 +0530

    Added filter by name and region

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,19 +1,51 @@
-import { Container, Grid, makeStyles } from "@material-ui/core";
+import {
+  Container,
+  FormControl,
+  Grid,
+  InputAdornment,
+  InputLabel,
+  makeStyles,
+  OutlinedInput,
+  Select,
+  Box,
+} from "@material-ui/core";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Country } from "../types/country";
 import CountryCard from "./CountryCard";
 import CountryDetails from "./CountryDetails";
+import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles({
   containerStyle: {
     paddingTop: "5vh",
   },
+  formControlStyle: {
+    marginBottom: "2%",
+  },
+  formStyle: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
 });
 
+interface inputStateDesign {
+  name: string;
+  region: string;
+}
+
 const CountryList = () => {
+  const emptyInputState = {
+    name: "",
+    region: "",
+  };
   const [countryList, setCountryList] = useState<Country[]>([]);
   const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
+  const [inputState, setInputState] = useState<inputStateDesign>(
+    emptyInputState
+  );
+  const [apiQuery, setApiQuery] = useState<string>("");
 
   const classes = useStyles();
 
@@ -24,12 +56,86 @@ const CountryList = () => {
       );
       setCountryList(countryListFromAPI.data);
     };
-    fetchCountryList();
+    const fetchFilteredCountryList = async () => {
+      const filteredCountryListFromAPI = await axios(
+        `https://restcountries.eu/rest/v2/${apiQuery}`
+      );
+      setCountryList(filteredCountryListFromAPI.data);
+    };
+    if (inputState.name === "" && inputState.region === "") {
+      fetchCountryList();
+    } else {
+      fetchFilteredCountryList();
+    }
     return () => {};
-  }, []);
+  }, [apiQuery]);
+
+  const handleChange = (
+    event: React.ChangeEvent<{ name?: string; value: unknown }>
+  ) => {
+    const name = event.target.name as keyof typeof inputState;
+    setInputState({
+      ...inputState,
+      [name]: event.target.value,
+    });
+    setApiQuery(`${name}/${event.target.value}`);
+  };
+
   return (
     <Container className={classes.containerStyle}>
       <Container>
+        <Box className={classes.formStyle}>
+          <FormControl
+            className={classes.formControlStyle}
+            variant="outlined"
+            style={{
+              width: "35%",
+            }}
+          >
+            <OutlinedInput
+              name="name"
+              id="outlined-adornment-amount"
+              type="text"
+              placeholder="Search for a country..."
+              value={inputState.name}
+              onChange={handleChange}
+              startAdornment={
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormControl
+            className={classes.formControlStyle}
+            variant="outlined"
+            style={{
+              width: "15%",
+            }}
+          >
+            <InputLabel htmlFor="outlined-filter-by-region-native-simple">
+              Filter by Region
+            </InputLabel>
+            <Select
+              native
+              value={inputState.region}
+              onChange={handleChange}
+              label="Filter by Region"
+              inputProps={{
+                name: "region",
+                id: "outlined-filter-by-region-native-simple",
+              }}
+              placeholder="Filter by Region"
+            >
+              <option aria-label="None" value="" />
+              <option value="Africa">Africa</option>
+              <option value="Americas">Americas</option>
+              <option value="Asia">Asia</option>
+              <option value="Europe">Europe</option>
+              <option value="Oceania">Oceania</option>
+            </Select>
+          </FormControl>
+        </Box>
         <Grid container spacing={4}>
           {selectedCountry ? (
             <CountryDetails
